059ae3b68d04673d993ed3e0acf657cf995201a8
Ricardo Augusto de Sousa Garcia da Silva <ricardoaugustogarcia@hotmail.com>
2020-07-09 17:47:29 -0300

    Update deleteCompany.php

--- controller/deleteCompany.php
+++ controller/deleteCompany.php
@@ -8,11 +8,13 @@ class DeleteCompanyController
     {
         $data = json_decode(file_get_contents("php://input"), true);
         $this->company = new Company();
-        if ($this->company->deleteCompany($data['id']) == true) {
-            echo "A empresa deletado com sucesso!";
-        } else {
-            echo "Erro ao deletar empresa!";
-        }
+        $result = $this->company->deleteCompany($data['id']);
+        echo $result;
+//         if ($result) {
+//             echo "A empresa deletado com sucesso!";
+//         } else {
+//             echo "Erro ao deletar empresa!";
+//         }
     }
 }
 new DeleteCompanyController();
