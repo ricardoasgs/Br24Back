8f328f61cae7c50c93d7b922bc905daddceb3df4
Ricardo Augusto de Sousa Garcia da Silva <ricardoaugustogarcia@hotmail.com>
2020-07-09 09:55:59 -0300

    Update getCompany.php

--- controller/getCompany.php
+++ controller/getCompany.php
@@ -1,8 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With");
 require_once "../model/company.php";
 class GetCompanyController
 {
