af429656e03d122e6620422c9dfcc293369e7e13
Ricardo Augusto de Sousa Garcia da Silva <ricardoaugustogarcia@hotmail.com>
2020-07-10 13:34:51 -0300

    Update createContact.php

--- controller/createContact.php
+++ controller/createContact.php
@@ -15,11 +15,12 @@ class CreateContactController
     {
         $data = json_decode(file_get_contents("php://input"), true);
         $result = $this->company->createContact($data);
-        if ($result) {
-            echo "O contato criado com sucesso!";
-        } else {
-            echo "Erro ao gravar Contato!";
-        }
+        echo $result;
+//         if ($result) {
+//             echo "O contato criado com sucesso!";
+//         } else {
+//             echo "Erro ao gravar Contato!";
+//         }
     }
 }
 new CreateContactController();
